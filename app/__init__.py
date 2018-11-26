02c26e7d783f9636bed026b4d656370c17e41532
katherine95 <kathiekim95@gmail.com>
2018-11-26 16:19:39 +0300

    [update]update python version on travis file

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+#from flask_restful import Api, Resource
+#from flask import Blueprints
+#from api.v1 import version_one as v1
+
+#def create_app():
+    #app = Flask()
+    #app.register_blueprint(v1)
+    #return app
