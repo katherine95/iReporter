0e199b6f8a1848166ebae2b75ee3da2a64c8bf0c
katherine95 <kathiekim95@gmail.com>
2018-12-03 01:06:15 +0300

    [ft #162355030]Add POST request

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,9 @@
+from flask import Flask
 from flask import Blueprint
 from flask_restful import Resource, Api
 
 from app.api.v1.views.incidents import Incidents, Incident, UpdateIncident
+from app.api.v1.views.users import Users
 
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(v1)
@@ -9,3 +11,4 @@ api = Api(v1)
 api.add_resource(Incidents, '/incidents')
 api.add_resource(Incident, '/incident/<id>')
 api.add_resource(UpdateIncident, '/incident/<id>/<attribute>')
+api.add_resource(Users, '/users')
