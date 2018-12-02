379ec9bc6afcfb0ba9a5a742e61d3b1d2d023a43
katherine95 <kathiekim95@gmail.com>
2018-12-02 09:53:25 +0300

    [ft 162298524]configure environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask, Blueprint
+from instance.config import app_config
+
 
 from app.api.v1 import v1 as version_one
 
-config_name = os.getenv('FLASK_ENV') # config_name = "development"
+config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
 def create_app(config_name=config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
     
     app.register_blueprint(version_one)
-    return app
-
+    return app    
+    
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
 from app import create_app
-from instance.config import app_config
 
-config_name = os.getenv('FLASK_ENV') # config_name = "development"
-app = create_app(app_config[config_name])
+config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
