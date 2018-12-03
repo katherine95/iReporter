72a48bff9e8725381a6979013c6b38ff2bda92b1
katherine95 <kathiekim95@gmail.com>
2018-12-03 16:55:28 +0300

    [ft #162369736]Update config environment

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+# config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
