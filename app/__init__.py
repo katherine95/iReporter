859141f57fe2a9e444ea94eee4d55e7ca6ec56c8
katherine95 <kathiekim95@gmail.com>
2018-12-06 00:05:28 +0300

    [starts #162460085]Register v2 api using blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,13 @@ from instance.config import DevelopmentConfig
 
 
 from app.api.v1 import v1 as version_one
-
-# config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+from app.api.v2 import v2 as version_two
 
 def create_app(config_name=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_name)
-    # app.config.from_pyfile('config.py')
     
     app.register_blueprint(version_one)
+    app.register_blueprint(version_two)
     return app    
     
