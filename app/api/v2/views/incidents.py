3f608208505cff6e7929b73fb2e6a75231f49219
katherine95 <kathiekim95@gmail.com>
2018-12-14 12:19:59 +0300

    [bg #162648890]Ensure a user edits on his/her own posted records

--- app/api/v2/views/incidents.py
+++ app/api/v2/views/incidents.py
@@ -140,37 +140,42 @@ class UpdateIncident(Resource):
     @jwt_required
     def patch(self, id):
         current_user = get_jwt_identity()
-        user = userObject.get_user_by_id(current_user)
-        res = incidentObject.get_incident_by_id(id)
-        if res:
-            if res['status'] == 'pending':
-                data = request.get_json()
-                if 'comment' in data:
-                    comment = data['comment']
-                    resp = incidentObject.update_incident_comment(id, comment)
+        user_record = incidentObject.get_incident_by_id(id)
+        if user_record['createdBy'] == current_user:
+            res = incidentObject.get_incident_by_id(id)
+            if res:
+                if res['status'] == 'pending':
+                    data = request.get_json()
+                    if 'comment' in data:
+                        comment = data['comment']
+                        resp = incidentObject.update_incident_comment(id, comment)
+                        return make_response(jsonify({
+                            "status": 200,
+                            "data": resp,
+                            "message": "Comment patched successfully"
+                        }), 200)
+                    elif 'location' in data:
+                        location = data['location']
+                        resp = incidentObject.update_incident_location
+                        (id, location)
+                        return make_response(jsonify({
+                            "status": 200,
+                            "data": resp,
+                            "message": "Comment patched successfully"
+                        }), 200)
                     return make_response(jsonify({
-                        "status": 200,
-                        "data": resp,
-                        "message": "Comment patched successfully"
-                    }), 200)
-                elif 'location' in data:
-                    location = data['location']
-                    resp = incidentObject.update_incident_location
-                    (id, location)
-                    return make_response(jsonify({
-                        "status": 200,
-                        "data": resp,
-                        "message": "Comment patched successfully"
-                    }), 200)
+                        "status": 400,
+                        "message": "Please provide 'comment' or 'location'"
+                    }), 400)
                 return make_response(jsonify({
-                    "status": 400,
-                    "message": "Please provide 'comment' or 'location'"
-                }), 400)
+                    "status": 401,
+                    "message": "You can only edit a record while its pending"
+                }), 401)
             return make_response(jsonify({
-                "status": 401,
-                "message": "You can only edit a record while its pending"
-            }), 401)
+                "status": 404,
+                "message": "Incident with that ID doesnt exist"
+            }), 404)
         return make_response(jsonify({
-            "status": 404,
-            "message": "Incident with that ID doesnt exist"
-        }), 404)
+            "status": 401,
+            "message": "You are only allowed to edit your own records"
+        }), 401)
