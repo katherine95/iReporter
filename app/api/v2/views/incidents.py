60bcd990aa4b045ddeeb75d21f1353b34af08a35
katherine95 <kathiekim95@gmail.com>
2018-12-14 13:25:28 +0300

    [bg #162648890]Ensure a user edits on his/her own posted records

--- app/api/v2/views/incidents.py
+++ app/api/v2/views/incidents.py
@@ -44,8 +44,7 @@ class Incidents(Resource):
                     }), 409)
             return make_response(jsonify({
                     "status": 400,
-                    "message": "incidentType should be 'Redflag'\
-                     or 'Intervention'"
+                    "message": "incidentType should be 'Redflag' or 'Intervention'"
                 }), 400)
         return make_response(jsonify({
             "status": 400,
@@ -149,8 +148,7 @@ class UpdateIncident(Resource):
                     data = request.get_json()
                     if 'comment' in data:
                         comment = data['comment']
-                        resp = incidentObject.update_incident_comment
-                        (id, comment)
+                        resp = incidentObject.update_incident_comment(id, comment)
                         return make_response(jsonify({
                             "status": 200,
                             "data": resp,
