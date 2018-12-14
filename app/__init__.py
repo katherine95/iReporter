b22b3d3b3064e1129a14eb6b5140186b28ceb162
katherine95 <kathiekim95@gmail.com>
2018-12-14 20:32:09 +0300

    [ch]Update Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ from app.api.v2 import v2 as version_two
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(app_config[os.getenv('APP_SETTINGS')])
+    # test = app.config.from_object(app_config[os.getenv('APP_SETTINGS')])
+    # print(test)
 
     app.config['JWT_SECRET_KEY'] = 'secret-key'
     app.config['PROPAGATE_EXCEPTIONS'] = True
