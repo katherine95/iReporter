d61a47f4e6a31cb8efc170270c46c1745cd54310
katherine95 <kathiekim95@gmail.com>
2019-01-10 15:51:30 +0300

    [starts #163080970]Add index.js file

---
+++ UI/assets/js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('login').addEventListener('click', login);
+// function login(){
+//     fetch('https://floating-reaches-50695.herokuapp.com/api/v2/auth/login')
+//     .then((response) => response.text())
+//     .then((data) => {
+
+//     })
+// }
