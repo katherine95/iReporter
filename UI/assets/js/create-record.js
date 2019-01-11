9e856c22ed7f8b2090bbdf9f2140e5578d4389e1
katherine95 <kathiekim95@gmail.com>
2019-01-11 15:04:53 +0300

    [finishes #163131897]Consume create incident endpoint

---
+++ UI/assets/js/create-record.js
@@ -0,0 +1,41 @@
+document.getElementById('createIncident').addEventListener('submit', createIncident);
+function createIncident(e){
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    let message = localStorage.getItem("message");
+    let comment = document.getElementById('comment').value;
+    let location = document.getElementById('location').value;
+    let incidentType = document.getElementById('incidentType').value;
+
+    fetch('https://floating-reaches-50695.herokuapp.com/api/v2/incidents', {
+        method:'POST',
+        headers:{
+            Accept:'application/json',
+            'Content-type':'application/json',
+            'mode':'cors',
+            Authorization: "Bearer " + token
+        },
+        body:JSON.stringify(
+            {
+                comment:comment, 
+                location:location,
+                incidentType:incidentType
+            })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if(data.status === 201){
+            console.log(data)
+            localStorage.setItem("message",data.msg);
+            window.location.replace('records.html')
+        }else if(message === "Token has expired"){
+            window.alert("Please log in");
+            window.location.replace('index.html')
+        }else{
+            console.log(data.msg)
+            localStorage.setItem("message",data.msg);
+            document.getElementById("alert").style.color = "red";
+            document.getElementById("alert").innerHTML = data.message;
+        }
+    })
+}
