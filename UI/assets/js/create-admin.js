1ab924570a392a8ed6719a5c6e06b7405a21947d
katherine95 <kathiekim95@gmail.com>
2019-01-17 10:38:41 +0300

    [finishes #163187405]Consume create admin endpoint

--- UI/assets/js/create-admin.js
+++ UI/assets/js/create-admin.js
@@ -1,35 +1,44 @@
-document.getElementById('createAdmin').addEventListener('click', createAdmin);
-function createAdmin(){
-    const token = localStorage.getItem("token");
-    let message = localStorage.getItem("message");
-    let url = new URL(window.location.href);
-    let userName = url.searchParams.get("userName");
+window.onload = function(){
+    createAdmin()
+        function createAdmin(){
+        const token = localStorage.getItem("token");
+        let message = localStorage.getItem("message");
+        let url = new URL(window.location.href);
+        let userName = url.searchParams.get("userName");
+        console.log(userName)
 
-    fetch(`https://floating-reaches-50695.herokuapp.com/api/v2/auth/users/${userName}`,{
-        method:'PATCH',
-        headers:{
-            Accept:'application/json',
-            'Content-type':'application/json',
-            'mode':'cors',
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Request-Method": "*",
-            Authorization: "Bearer " + token
-        }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        if(data.status === 200){
-            window.alert("Admin created Successfully")
-            window.location.replace('users-list.html')
-        // }else if(message === "Token has expired"){
-        //     window.alert("Please log in");
-        //     window.location.replace('index.html')
-        }else{
-            console.log(data.msg)
-            localStorage.setItem("message",data.msg);
-            document.getElementById("alert").style.color = "red";
-            document.getElementById("alert").innerHTML = data.message;
-        }
-    })
+        fetch(`https://floating-reaches-50695.herokuapp.com/api/v2/auth/users/${userName}`,{
+            method:'PATCH',
+            headers:{
+                Accept:'application/json',
+                'Content-type':'application/json',
+                'mode':'cors',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Request-Method": "*",
+                Authorization: "Bearer " + token
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if(data.status === 200){
+                window.alert("Admin created Successfully")
+                window.location.replace('users-list.html')
+            }else if(message === "Token has expired"){
+                window.alert("Please log in");
+                window.location.replace('index.html');
+            }else if(data.message="User is already an Admin"){
+                window.alert("User is already an Admin");
+                window.location.replace('users-list.html')
+            }else if(data.message="you dont have access rights"){
+                window.alert("You are not an admin");
+                window.location.replace('index.html')
+            }else{
+                console.log(data.msg)
+                localStorage.setItem("message",data.msg);
+                document.getElementById("alert").style.color = "red";
+                document.getElementById("alert").innerHTML = data.message;
+            }
+        })
+    }
 }
