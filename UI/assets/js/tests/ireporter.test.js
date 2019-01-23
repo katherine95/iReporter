cce6a46df563f3bd8ba0eaa3e5be76a224d54d0d
katherine95 <kathiekim95@gmail.com>
2019-01-23 19:15:36 +0300

    [ch]Add coverage directory

--- UI/assets/js/tests/ireporter.test.js
+++ UI/assets/js/tests/ireporter.test.js
@@ -19,7 +19,7 @@ const user = {
 };
 
 const record = {
-  comment: 'This is a report concerning waterhfg jg jdhgd jhsd hjfhgf gjh ghjg ggjhgkj hsjha',
+  comment: 'This is a report concerning waterhfg jgfgfg sjhdk kjhdkjjjj ghjg ggjhgkj hsjha',
   incidentType: 'Redflag',
   location: '45N',
   image: faker.image.imageUrl(),
