48cec08c0e2f6887ad7db4f4616407da981325ce
katherine95 <kathiekim95@gmail.com>
2019-01-25 17:37:16 +0300

    [ch #163403031]Fix eslint errors

--- UI/assets/js/create-record.js
+++ UI/assets/js/create-record.js
@@ -1,62 +1,60 @@
 document.getElementById('createIncident').addEventListener('submit', createIncident);
-function createIncident(e){
-    e.preventDefault();
-    const token = localStorage.getItem("token");
-    let message = localStorage.getItem("message");
-    let comment = document.getElementById('comment').value;
-    let location = document.getElementById('location').value;
-    let incidentType = document.getElementById('incidentType').value;
-    let image = document.getElementById('custom-text').innerHTML;
+function createIncident(e) {
+  e.preventDefault();
+  const token = localStorage.getItem('token');
+  const message = localStorage.getItem('message');
+  const comment = document.getElementById('comment').value;
+  const location = document.getElementById('location').value;
+  const incidentType = document.getElementById('incidentType').value;
+  const image = document.getElementById('custom-text').innerHTML;
 
-    fetch('https://floating-reaches-50695.herokuapp.com/api/v2/incidents', {
-        method:'POST',
-        headers:{
-            Accept:'application/json',
-            'Content-type':'application/json',
-            'mode':'cors',
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Request-Method": "*",
-            Authorization: "Bearer " + token
-        },
-        body:JSON.stringify(
-            {
-                comment:comment, 
-                location:location,
-                incidentType:incidentType,
-                image:[image]
-            })
-    })
-    .then((response) => response.json())
+  fetch('https://floating-reaches-50695.herokuapp.com/api/v2/incidents', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      mode: 'cors',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Request-Method': '*',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(
+      {
+        comment,
+        location,
+        incidentType,
+        image: [image],
+      },
+    ),
+  })
+    .then(response => response.json())
     .then((data) => {
-        console.log(data);
-        if(data.status === 201){
-            console.log(data)
-            localStorage.setItem("message",data.msg);
-            window.location.replace('user-account.html')
-        }else if(message === "Token has expired"){
-            window.alert("Please log in");
-            window.location.replace('index.html')
-        }else{
-            console.log(data.msg)
-            localStorage.setItem("message",data.msg);
-            document.getElementById("alert").style.color = "red";
-            document.getElementById("alert").innerHTML = data.message;
-        }
-    })
+      if (data.status === 201) {
+        localStorage.setItem('message', data.msg);
+        window.location.replace('user-account.html');
+      } else if (message === 'Token has expired') {
+        window.alert('Please log in');
+        window.location.replace('index.html');
+      } else {
+        localStorage.setItem('message', data.msg);
+        document.getElementById('alert').style.color = 'red';
+        document.getElementById('alert').innerHTML = data.message;
+      }
+    });
 }
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
+const realFileBtn = document.getElementById('real-file');
+const customBtn = document.getElementById('custom-button');
+const customTxt = document.getElementById('custom-text');
 
-window.addEventListener('load', function() {
-    customBtn.addEventListener("click", function() {
-        realFileBtn.click();
-    });
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (realFileBtn.value) {
-            var img = document.querySelector('img');  // $('img')[0]
-            img.src = URL.createObjectURL(this.files[0]); // set src to file url
-            customTxt.innerHTML = img.src;
-        }
-    });
+window.addEventListener('load', () => {
+  customBtn.addEventListener('click', () => {
+    realFileBtn.click();
   });
+  document.querySelector('input[type="file"]').addEventListener('change', function () {
+    if (realFileBtn.value) {
+      const img = document.querySelector('img'); // $('img')[0]
+      img.src = URL.createObjectURL(this.files[0]); // set src to file url
+      customTxt.innerHTML = img.src;
+    }
+  });
+});
--- UI/assets/js/users-list.js
+++ UI/assets/js/users-list.js
@@ -1,59 +1,58 @@
-window.onload = function(){
-    getUsers();
+window.onload = function () {
+  getUsers();
 
-    function getUsers(){
-        const token = localStorage.getItem("token");
-        let message = localStorage.getItem("message");
-    
-        fetch('https://floating-reaches-50695.herokuapp.com/api/v2/auth/signup',{
-        headers:{
-            Accept:'application/json',
-            'Content-type':'application/json',
-            'mode':'cors',
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Request-Method": "*",
-            Authorization: "Bearer " + token
+  function getUsers() {
+    const token = localStorage.getItem('token');
+    const message = localStorage.getItem('message');
+
+    fetch('https://floating-reaches-50695.herokuapp.com/api/v2/auth/signup', {
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+        mode: 'cors',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': '*',
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data.data);
+        if (data.status === 200) {
+          const table = document.getElementById('users');
+          const users = data.data;
+          users.map((user) => {
+            const new_row = table.insertRow();
+
+            const id = new_row.insertCell(0);
+            const email = new_row.insertCell(1);
+            const firstname = new_row.insertCell(2);
+            const lastname = new_row.insertCell(3);
+            const othernames = new_row.insertCell(4);
+            const phonenumber = new_row.insertCell(5);
+            const createdOn = new_row.insertCell(6);
+            const username = new_row.insertCell(7);
+            const isAdmin = new_row.insertCell(8);
+            const viewUser = new_row.insertCell(9);
+
+            id.innerHTML = users.indexOf(user) + 1;
+            email.innerHTML = user.email;
+            firstname.innerHTML = user.firstname;
+            lastname.innerHTML = user.lastname;
+            othernames.innerHTML = user.othernames;
+            phonenumber.innerHTML = user.phonenumber;
+            createdOn.innerHTML = user.createdOn;
+            username.innerHTML = user.username;
+            isAdmin.innerHTML = user.isAdmin;
+            viewUser.innerHTML = `<a href='create-admin.html?userName=${ user.username }' id='makeAdmin'>Make Admin</a>`;
+          });
+        } else if (message === 'Token has expired') {
+          window.alert('Please log in');
+          window.location.replace('index.html');
+        } else{
+          document.getElementById('alert').style.color = 'red';
+          document.getElementById('alert').innerHTML = data.message;
         }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.data)
-            if(data.status === 200){
-                let table = document.getElementById("users");
-                let users = data.data;
-                users.map((user) => {
-                    let new_row = table.insertRow();
-                    
-                    let id = new_row.insertCell(0);
-                    let email = new_row.insertCell(1);
-                    let firstname = new_row.insertCell(2);
-                    let lastname = new_row.insertCell(3);
-                    let othernames = new_row.insertCell(4);
-                    let phonenumber = new_row.insertCell(5);
-                    let createdOn = new_row.insertCell(6);
-                    let username = new_row.insertCell(7);
-                    let isAdmin = new_row.insertCell(8);
-                    let viewUser = new_row.insertCell(9);
-
-                    id.innerHTML = users.indexOf(user) + 1;
-                    email.innerHTML = user.email;
-                    firstname.innerHTML = user.firstname;
-                    lastname.innerHTML =user.lastname;
-                    othernames.innerHTML =user.othernames;                 
-                    phonenumber.innerHTML = user.phonenumber;
-                    createdOn.innerHTML = user.createdOn;
-                    username.innerHTML = user.username;
-                    isAdmin.innerHTML = user.isAdmin;
-                    viewUser.innerHTML = "<a href='create-admin.html?userName="+ user.username +"' id='makeAdmin'>Make Admin</a>";
-                });
-            }else if(message === "Token has expired"){
-                window.alert("Please log in");
-                window.location.replace('index.html')
-            }else{
-                document.getElementById("alert").style.color = "red";
-                document.getElementById("alert").innerHTML = data.message;
-            }
-        })
-    }
-}
-
+      });
+  }
+};
